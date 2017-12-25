c3225cd31ffde9ba31697e3a644135c4b2565cd9
Kliment-Lambevski <kliment.lambevski@gmail.com>
2017-12-25 22:23:41 +0100

    fix(input) allow larger text

--- src/components/ip-input/ip-input.vue
+++ src/components/ip-input/ip-input.vue
@@ -2,12 +2,27 @@
   <div class="ip-input-container">
     <label>
       <input
+        v-if="!disabled"
         :type="type"
         :placeholder="placeholder"
         :disabled="disabled"
         :value="value"
         @input="$emit('input', $event.target.value)"
         class="ip-input">
+      <div class="ip-input-disabled" v-else>
+        {{value}}
+      </div>
+      <!--<textarea-->
+        <!--v-else-->
+        <!--:placeholder="placeholder"-->
+        <!--:disabled="disabled"-->
+        <!--:value="value"-->
+        <!--@input="emitInput"-->
+        <!--class="ip-input"-->
+        <!--rows="1"-->
+        <!--style="overflow:hidden">-->
+
+      </textarea>
     </label>
     <div class="error-msg" v-if="errorMsg">{{errorMsg}}</div>
   </div>
@@ -33,6 +48,13 @@
       },
       errorMsg: {
 
+      }
+    },
+    methods: {
+      emitInput(e) {
+        this.$emit('input', e.target.value);
+        e.target.style.height = "5px";
+        e.target.style.height = ( e.target.scrollHeight)+"px";
       }
     },
   }
